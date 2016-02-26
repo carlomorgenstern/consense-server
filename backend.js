e056e5aff29989510cf9e8fc85f3edd38b3bb5a8
Philipp <pk@philippkoch.com>
2016-02-26 21:59:38 +0100

    Download lädt nur nicht-leere ICS-Dateien herunter. Läuft durch.

--- backend.js
+++ backend.js
@@ -9,30 +9,24 @@ var url = require('url');
 var http = require('http');
 
 // download something from a given url and save it on disk
-var downloadFromUrl = function(fileUrl, destinationDir) {
-	var DOWNLOAD_DIR = destinationDir;
+var downloadFromUrl = function(fileUrl, destinationDir, file_name) {
+	var DOWNLOAD_DIR = destinationDir;	
+	var fileChunks = [];
+	
+	http.get(fileUrl, function(res) {
+		res.on('data', function(data) {			 						
+			fileChunks.push(data);
+		}).on('end', function() {
+			var completeFile = fileChunks.join(''); // put together all the chunks in one string
+			if (!completeFile.toString().startsWith('<script type="text/javascript">')) {
+				var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
+				file.write(completeFile);
+			}			
+		});
+	});
+};	
 
-	// Function to download file using HTTP.get	
-	var options = {
-		host: url.parse(fileUrl).host,
-		port: 80,
-		path: url.parse(fileUrl).pathname
-	};
-
-	var file_name = url.parse(fileUrl).pathname.split('/').pop();
-	var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
-
-	http.get(options, function(res) {
-		res.on('data', function(data) {
-				file.write(data);
-			}).on('end', function() {
-				file.end();
-				console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
-			});
-		});	
-};
-
-
+	
 // Define what happens if someone requests something from the server
 app.get('/', function(req, res) {
 	// res.send("Lese die Studiengänge ein (vgl. Console)"); // DEBUG
@@ -40,7 +34,7 @@ app.get('/', function(req, res) {
 	// Fill array with courses (Studiengänge) from text file
 	var majorNames = []				// holds majors whose schedules are to be fetched
 	var fs = require('fs'); 				// filesystem
-  	var filename = './courseData/majors.txt';
+  	var filename = './courseData/_majors.txt';
 	
 	var majorNames = fs.readFileSync(filename, 'utf8').split(";");
 	console.log('Faecher: ' + majorNames);
@@ -48,22 +42,16 @@ app.get('/', function(req, res) {
 	var courses = ["a", "b", "c"];	// courses in a given major, e.g. WI13a, WI13b etc.
 	var numberOfSemesters = 6;	// how many semester are to be crawled per major and majors
 	var icsMoodleUrlBase = 'http://moodle.hwr-berlin.de/fb2-stundenplan/download.php?doctype=.ics&url=./fb2-stundenplaene/';
-	var icsUrls = [];
-		
+			
 	for (i in majorNames) {
 		for (var sem=1; sem<numberOfSemesters+1; sem++) {
 			for (j in courses) {
-				// Append respective ICS-URL. Pattern: {baseURL}+{major}+"/semester"+{integer}+"/kurs"+{courseLetter}
-				icsUrls.push(icsMoodleUrlBase + majorNames[i] + '/semester' + sem + '/kurs' + courses[j]);
+				// Download ICS files: Pattern = {baseURL}+{major}+"/semester"+{integer}+"/kurs"+{courseLetter}
+				downloadFromUrl(icsMoodleUrlBase + majorNames[i] + '/semester' + sem + '/kurs' + courses[j], "./courseData/", majorNames[i]+sem+courses[j]+".ics");				
 			}
 		}
-	}
+	}	
 	
-	console.log("URLS: " + icsUrls); // DEBUG	
-	console.log(''); // DEBUG	
-	
-	// downloadFromUrl('http://www.hacksparrow.com/using-node-js-to-download-files.html', './courseData/');
-	downloadFromUrl(icsUrls[87], './courseData/');	
 });
 
 //  Start the server
